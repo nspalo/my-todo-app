4789f824cf82d4b332fc89f9364a2b93725b36b5
Noel Palo <noelspalo@gmail.com>
2023-09-29 23:25:21 +0800

    feature/MTDA-2-refactor - New UI - Laravel Breeze - Tailwind Css - Vue 3, Vue Loader, Vue Router - Add base scaffolding

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', static function () {
+Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
